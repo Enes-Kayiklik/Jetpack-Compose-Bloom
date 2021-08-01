6f638cbff9eb1b0df897fa86a692592ae4fb81da
eneskayiklik <enes@mikigi.com>
2021-08-01 18:55:22 +0300

    added colors

--- app/src/main/java/com/eneskayiklik/bloom/ui/theme/Color.kt
+++ app/src/main/java/com/eneskayiklik/bloom/ui/theme/Color.kt
@@ -2,7 +2,11 @@ package com.eneskayiklik.bloom.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Pink100 = Color(0xFFFFF1F1)
+val Pink900 = Color(0xFF3F1C1C)
+val White = Color(0xFFFFFFFF)
+val White850 = Color(0x85FFFFFF)
+val Gray = Color(0xFF232323)
+val Green900 = Color(0xFF2D3B2D)
+val Green300 = Color(0xFFB8C9B8)
+val White150 = Color(0x15FFFFFF)
